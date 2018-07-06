348bd643489ac75dd6ac12eb4d57dd8b2cef77db
Lưu Tuấn Hải <hai.luut@gmail.com>
2018-07-06 10:29:22 +0700

    Comment out visdom

--- util/visualizer.py
+++ util/visualizer.py
@@ -57,9 +57,9 @@ class Visualizer():
         self.opt = opt
         self.saved = False
         if self.display_id > 0:
-            import visdom
+            # import visdom
             self.ncols = opt.display_ncols
-            self.vis = visdom.Visdom(server=opt.display_server, port=opt.display_port)
+            # self.vis = visdom.Visdom(server=opt.display_server, port=opt.display_port)
 
         if self.use_html:
             self.web_dir = os.path.join(opt.checkpoints_dir, opt.name, 'web')
@@ -106,17 +106,17 @@ class Visualizer():
                 if label_html_row != '':
                     label_html += '<tr>%s</tr>' % label_html_row
                 # pane col = image row
-                self.vis.images(images, nrow=ncols, win=self.display_id + 1,
-                                padding=2, opts=dict(title=title + ' images'))
+                # self.vis.images(images, nrow=ncols, win=self.display_id + 1,
+                #                 padding=2, opts=dict(title=title + ' images'))
                 label_html = '<table>%s</table>' % label_html
-                self.vis.text(table_css + label_html, win=self.display_id + 2,
-                              opts=dict(title=title + ' labels'))
+                # self.vis.text(table_css + label_html, win=self.display_id + 2,
+                #               opts=dict(title=title + ' labels'))
             else:
                 idx = 1
                 for label, image in visuals.items():
                     image_numpy = util.tensor2im(image)
-                    self.vis.image(image_numpy.transpose([2, 0, 1]), opts=dict(title=label),
-                                   win=self.display_id + idx)
+                    # self.vis.image(image_numpy.transpose([2, 0, 1]), opts=dict(title=label),
+                    #                win=self.display_id + idx)
                     idx += 1
 
         if self.use_html and (save_result or not self.saved):  # save images to a html file
@@ -146,15 +146,15 @@ class Visualizer():
             self.plot_data = {'X': [], 'Y': [], 'legend': list(losses.keys())}
         self.plot_data['X'].append(epoch + counter_ratio)
         self.plot_data['Y'].append([losses[k] for k in self.plot_data['legend']])
-        self.vis.line(
-            X=np.stack([np.array(self.plot_data['X'])] * len(self.plot_data['legend']), 1),
-            Y=np.array(self.plot_data['Y']),
-            opts={
-                'title': self.name + ' loss over time',
-                'legend': self.plot_data['legend'],
-                'xlabel': 'epoch',
-                'ylabel': 'loss'},
-            win=self.display_id)
+        # self.vis.line(
+        #     X=np.stack([np.array(self.plot_data['X'])] * len(self.plot_data['legend']), 1),
+        #     Y=np.array(self.plot_data['Y']),
+        #     opts={
+        #         'title': self.name + ' loss over time',
+        #         'legend': self.plot_data['legend'],
+        #         'xlabel': 'epoch',
+        #         'ylabel': 'loss'},
+        #     win=self.display_id)
 
     # losses: same format as |losses| of plot_current_losses
     def print_current_losses(self, epoch, i, losses, t, t_data):
